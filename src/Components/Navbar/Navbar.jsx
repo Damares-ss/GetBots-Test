14756d848a5641ecc0a2240e2e3d7f897930cd10
Damares Soares da Silva <damares.s.bio@gmail.com>
2022-02-11 00:30:41 -0300

    Creating navbar components

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import {
+    Container,
+    Nav,
+    Navbar as BaseNavbar
+} from "react-bootstrap";
+import "./Navbar.css"
+
+
+const Navbar = () => {
+    return (
+        <BaseNavbar className="navbar">
+            <Container>
+                <BaseNavbar.Brand href="#" className="logo">GetbotsTest</BaseNavbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link className="link" href="https://getbots.com.br/">GetBots page</Nav.Link>
+                    <Nav.Link className="link" href="https://app.sheetlabs.com/#/services/doc/GETB/apidesafio">API utilizada</Nav.Link>
+                </Nav>
+            </Container>
+        </BaseNavbar>
+    )
+}
+export default Navbar
