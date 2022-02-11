702fae7c99dcc253d716a2b6ee851b2aae84fb58
Damares Soares da Silva <damares.s.bio@gmail.com>
2022-02-11 00:49:08 -0300

    Modifying color and font of navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     return (
         <BaseNavbar className="navbar">
             <Container>
-                <BaseNavbar.Brand href="#" className="logo">GetbotsTest</BaseNavbar.Brand>
+                <BaseNavbar.Brand href="#" className="logo"><b>GetbotsTest</b></BaseNavbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link className="link" href="https://getbots.com.br/">GetBots page</Nav.Link>
                     <Nav.Link className="link" href="https://app.sheetlabs.com/#/services/doc/GETB/apidesafio">API utilizada</Nav.Link>
