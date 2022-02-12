10d9dc8d557bdfafd39ec08eee8d68f273c1af57
Damares Soares da Silva <damares.s.bio@gmail.com>
2022-02-11 22:15:31 -0300

    Creating component Jumbotron

---
+++ src/Components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,14 @@
+import "./Jumbotron.css"
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid text-white mt-5">
+        <div className="container">
+          <h1 className="display-4">Seja bem-vindo ao GetBots Test</h1>
+          <p className="lead">Site desenvolvido pela web-developer Damares Soares. </p>
+        </div>
+      </div>
+  )
+}
+
+export default Jumbotron
