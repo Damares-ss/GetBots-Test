afd99530bbdc0fff6e086dae2bfa1357a5519271
Damares Soares da Silva <damares.s.bio@gmail.com>
2022-02-11 21:43:10 -0300

    Importing getUsers and creating states and effects

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,3 +1,5 @@
+import { getUsers } from "../../Api/users"
+import { useEffect, useState } from "react"
 import {
     Col,
     Container,
@@ -8,45 +10,24 @@ import Card from "../../Components/Card/Card"
 import "./Home.css"
 
 const Home = () => {
-    const cards = [
-        {
-            "__id": 2,
-            "nome": "Gabriel",
-            "email": "teste@teste.com.br",
-            "endereco": "Rua São Paulo, 1",
-            "telefone": 999999999
-        },
-        {
-            "__id": 4,
-            "nome": "Luiz",
-            "email": "a@a.com.br",
-            "endereco": "Rua Rio de Janeiro, 2",
-            "telefone": 888888888
-        },
-        {
-            "__id": 6,
-            "nome": "Antônio",
-            "email": "b@b.com.br",
-            "endereco": "Rua Salvador, 3",
-            "telefone": 777777777
-        },
-        {
-            "__id": 8,
-            "nome": "Sebastião",
-            "email": "c@c.com.br",
-            "endereco": "Rua Florianópolis, 4",
-            "telefone": 666666666
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsersData = async () => {
+            const res = await getUsers()
+            setUsers(res)
         }
-    ]
+        getUsersData()
+    }, [])
 
     return (
         <div className="home">
             <Navbar />
             <Container>
                 <Row>
-                    {cards.map((c) => (
+                    {users.map((u) => (
                         <Col className="mt-5" sm={4}>
-                            <Card key={c._id} card={c} />
+                            <Card key={u._id} card={u} />
                         </Col>
                     ))}
                 </Row>
